1e2aa5c0f4b5f7822efc7f545cc7516306144758
Zeshan Amjad <zeshan@zesh.me>
2019-01-26 13:41:46 +0000

    Add subscription to Pusher channel

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import Pusher from "pusher-js";
 
 const PUSHER_APP_KEY = "fa06261efc5349a70ad5";
 const PUSHER_APP_CLUSTER = "eu";
+const PUSHER_POUR_CHANNEL = "pour";
 
-console.log(Pusher, PUSHER_APP_KEY, PUSHER_APP_CLUSTER);
+const socket = new Pusher(PUSHER_APP_KEY, {
+  cluster: PUSHER_APP_CLUSTER
+});
+
+const pourChannel = socket.subscribe(PUSHER_POUR_CHANNEL);
+
+pourChannel.bind_global((event, data) => {
+  console.log(`Received event: ${event}, with data: ${data}`);
+});

b843159445c5377cb968b20730d1ca4559be8a08
Zeshan Amjad <zeshan@zesh.me>
2019-01-26 13:33:33 +0000

    Add pre-commit hooks for ESLint validation

--- index.js
+++
@@ -1,4 +0,0 @@
-import Pusher from 'pusher-js'
-
-const PUSHER_APP_KEY = "fa06261efc5349a70ad5";
-const PUSHER_APP_CLUSTER = "eu";
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+import Pusher from "pusher-js";
+
+const PUSHER_APP_KEY = "fa06261efc5349a70ad5";
+const PUSHER_APP_CLUSTER = "eu";
+
+console.log(Pusher, PUSHER_APP_KEY, PUSHER_APP_CLUSTER);
